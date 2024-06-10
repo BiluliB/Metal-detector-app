adeb32424e30c58db192bde8f54d5aaddb35fe49
Robin <39103327+RobinRuf@users.noreply.github.com>
2024-06-10 22:42:27 +0200

    Finished AboutPage

--- Magnetify/ViewModels/AboutViewModel.cs
+++ Magnetify/ViewModels/AboutViewModel.cs
@@ -4,10 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Magnetify.ViewModels
 {
     public class AboutViewModel : BaseNotifyHandler
     {
+        public ICommand OpenUrlCommand { get; }
+
+        public AboutViewModel()
+        {
+            OpenUrlCommand = new Command<string>(async (url) => await OpenUrl(url));
+        }
+
+        private async Task OpenUrl(string url)
+        {
+            if (!Uri.TryCreate(url, UriKind.Absolute, out var uri))
+            {
+                return;
+            }
+
+            await Launcher.OpenAsync(uri);
+        }
     }
 }
